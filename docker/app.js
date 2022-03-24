41973f797b44bcb00e3cc63429a41ae0d456958b
Oluebube <onuohaoluebube05@gmail.com>
2022-03-24 07:55:41 +0100

    Modified web page message

--- docker/app.js
+++ docker/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.write('Welcome to SCA Cloud School Application')
+        res.write('Welcome to SCA Cloud School Application , this is my first assessment')
         res.end()
     }   
 
